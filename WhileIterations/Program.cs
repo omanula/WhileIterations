791a97cf7b1770fbfc0f28c9d6d9dbda5dd7101a
Olivia <31357493+omanula@users.noreply.github.com>
2017-09-26 10:31:15 -0600

    While Iterations

---
+++ WhileIterations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace WhileIterations
+{
+    class MainClass
+    {
+        // Tell Visual Studios to copy the Values.txt file into the project bin
+        public static void Main(string[] args)
+        {
+            // StreamReader doesn't live in the default name spaces (in system.io)
+            // Sucks in data little by little from Values.txt file
+            StreamReader myReader = new StreamReader("Values.txt");
+            string line = "";
+
+            // Null value = unknown/indeterminate value
+            // While - inside determines whether to continue to cancel out
+            while (line != null)
+            {
+                line = myReader.ReadLine();
+                if (line != null)
+                    Console.WriteLine(line);
+            }
+
+            myReader.Close();
+            // failure to close the file can leave a reference to the file open
+            // and prevent other users/applications from accessing the file
+            Console.ReadLine();
+        }
+    }
+    // Important Concepts
+
+    // While Iteration statement when there is an indeterminate times to iterate
+    // Stream Reader class - suck in data line by line from text using ReadLine
+    // Close connection/hold of text file
+    // Provide using statement for other classes
+}
